1c648946875b0e4edbc2d78b4dd3f83d4aacca55
AlphaZer00 <batudasdemir@gmail.com>
2025-04-21 12:21:28 -0400

    Add example test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-import "./App.css";
+import { useState } from "react";
 
-const App = () => <h1>First Test</h1>;
+const App = () => {
+    const [heading, setHeading] = useState("Magnificent Monkeys");
+
+    const handleClick = () => {
+        setHeading("Radical Rhinos");
+    };
+
+    return (
+        <>
+            <button type="button" onClick={handleClick}>
+                Click here
+            </button>
+            <h1>{heading}</h1>
+        </>
+    );
+};
 
 export default App;
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 import { describe, it, expect } from "vitest";
+import userEvent from "@testing-library/user-event";
 
 describe("App component", () => {
-    it("renders heading correctly", () => {
+    it("renders Magnificent Monkeys", () => {
+        const { container } = render(<App />);
+        expect(container).toMatchSnapshot();
+    });
+
+    it("renders radical rhinos after button click", async () => {
+        const user = userEvent.setup();
+
         render(<App />);
-        expect(screen.getByRole("heading").textContent).toMatch(/First Test/i);
+        const button = screen.getByRole("button", { name: "Click here" });
+
+        await user.click(button);
+
+        expect(screen.getByRole("heading").textContent).toMatch(
+            /Radical Rhinos/i
+        );
     });
 });
